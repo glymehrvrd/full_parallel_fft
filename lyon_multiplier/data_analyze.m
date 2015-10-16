4fd2330ab9123c90a45c2f42027bde8458e8f484
glyme <glymehrvrd@gmail.com>
2015-10-16 15:22:53 +0800

    add comment.

--- lyon_multiplier/data_analyze.m
+++ lyon_multiplier/data_analyze.m
@@ -1,10 +1,13 @@
+% set number of input data
 d_num=4;
 d=simout.data(19:19+16*d_num-1);
 
+% retrieve calculation result from serial output
 p=zeros(d_num,16);
 results=zeros(d_num,1);
 for i=1:d_num
     p(i,:)=d(1+16*(i-1):16*i);
+    % different approach for +/- data
     if p(i,end)==0
         results(i)=bi2de(p(i,:));
     else
--- lyon_multiplier/data_gen.m
+++ lyon_multiplier/data_gen.m
@@ -1,13 +1,19 @@
 clear all;
 
+% input data
 d=[-15014, 15221, -5013, 10284]';
+
+% calculate the should-be result
 result1=bitshift(d*(-1061),-15,'int32');
 
+% convert data to signed fixed-point num
 d=sfi(d,16,0);
 
+% convert input data to serial stream, lsb outputs first
 d_bool=zeros(size(d,1),16);
 for i=1:size(d,1)
     tmp=d(i);
+    % due to multiplier property, 1st bit(msb) must equal to 2nd bit
     if tmp.bin(1)~=tmp.bin(2)
         error(['Input data overflows: ' num2str(tmp.dec)]);
     end;
@@ -17,11 +23,12 @@ d_bool=d_bool';
 d_bool=d_bool(:);
 d_bool=boolean(d_bool);
 
+% d1_in, starts at time 2
 t=0:numel(d_bool)+2;
 t=t';
-
 d1_in=timeseries([0;0; d_bool; 0],t);
 
+% ctrl signal
 d_ctrl=repmat([1 zeros(1,15)],length(d)+10,1);
 d_ctrl=d_ctrl';
 d_ctrl=boolean(d_ctrl(:));
