6f5e295534b2d5afc1f8992824ce7565d48dbfec
glyme <glymehrvrd@gmail.com>
2015-11-18 11:21:54 +0800

    reconstruct fft4 in fix point simulation, using the same architecture as the hardware.

--- fixpoint_matlab_simulator/fft4.m
+++ fixpoint_matlab_simulator/fft4.m
@@ -1,15 +1,28 @@
 function [result]=fft4(data)
-    r=real(data);
-    i=imag(data);
-    result(1)=complex(r(1)+r(2)+r(3)+r(4)...
-        ,i(1)+i(2)+i(3)+i(4));
+%     r=real(data);
+%     i=imag(data);
+%     result(1)=complex(r(1)+r(2)+r(3)+r(4)...
+%         ,i(1)+i(2)+i(3)+i(4));
+% 
+%     result(2)=complex(r(1)+i(2)-r(3)-i(4)...
+%         ,i(1)-r(2)-i(3)+r(4));
+% 
+%     result(3)=complex(r(1)-r(2)+r(3)-r(4)...
+%         ,i(1)-i(2)+i(3)-i(4));
+% 
+%     result(4)=complex(r(1)-i(2)-r(3)+i(4)...
+%         ,i(1)+r(2)-i(3)-r(4));
 
-    result(2)=complex(r(1)+i(2)-r(3)-i(4)...
-        ,i(1)-r(2)-i(3)+r(4));
+    for i=1:2
+        left_outputs(i,:)=fft2(data(i:2:end));
+    end;
+    left_outputs=left_outputs.';
 
-    result(3)=complex(r(1)-r(2)+r(3)-r(4)...
-        ,i(1)-i(2)+i(3)-i(4));
+    right_inputs=left_outputs;
+    right_inputs(4)=complex(imag(left_outputs(4)),-real(left_outputs(4)));
 
-    result(4)=complex(r(1)-i(2)-r(3)+i(4)...
-        ,i(1)+r(2)-i(3)-r(4));
+    for j=1:2
+        right_outputs(j,:)=fft2(right_inputs(j,:));
+    end;
+    result=right_outputs(:);
 end
